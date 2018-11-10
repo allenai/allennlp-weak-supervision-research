d188e1046b369e3a2eea42701e48a194915a3d34
Shikhar Murty <shikhar.murty@gmail.com>
2018-11-10 01:01:00 -0500

    fix silly bug in __init__

--- weak_supervision/models/__init__.py
+++ weak_supervision/models/__init__.py
@@ -1,4 +1,4 @@
 from weak_supervision.models.latent_alignment import LatentAlignment
-from weak_supervision.models.latent_alignment_char import LatentAlignmentSharedChar
+from weak_supervision.models.latent_alignment_shared_char import LatentAlignmentSharedChar
 from weak_supervision.models.semantic_parsing.wikitables_variable_free import (
         WikiTablesVariableFreeMml, WikiTablesVariableFreeErm)

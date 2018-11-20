299fb884906baf8766f0f3c33d05eeb3a01e2903
MurtyShikhar <shikhar.murty@gmail.com>
2018-11-20 17:17:00 -0500

    fix pylint

--- tests/data/dataset_readers/latent_alignment_test.py
+++ tests/data/dataset_readers/latent_alignment_test.py
@@ -1,8 +1,8 @@
 # pylint: disable=invalid-name,no-self-use
 
 from allennlp.common.testing import AllenNlpTestCase
-from weak_supervision.data.dataset_readers.semantic_parsing.latent_alignment import LatentAlignmentDatasetReader
 from allennlp.common import Params
+from weak_supervision.data.dataset_readers.semantic_parsing.latent_alignment import LatentAlignmentDatasetReader
 
 class TestLatentAlignmentDatasetReader(AllenNlpTestCase):
     def test_reader_can_read(self):

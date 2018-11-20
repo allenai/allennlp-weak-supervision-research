a9aa40e6b39476ba99456a9f730408454ca083e6
MurtyShikhar <shikhar.murty@gmail.com>
2018-11-20 16:02:24 -0500

    add tests

--- tests/models/latent_alignment_shared_char_test.py
+++ tests/models/latent_alignment_shared_char_test.py
@@ -6,7 +6,7 @@ from weak_supervision import *
 class LatentAlignmentSharedCharModelTest(ModelTestCase):
     def setUp(self):
         super().setUp()
-        self.set_up_model("fixtures/latent_alignment/experiment.json",
+        self.set_up_model("fixtures/latent_alignment/experiment_shared_char.json",
                           "fixtures/data/wikitables/alignment_preprocessed.json")
 
     def test_model_can_train_save_and_load(self):
--- tests/models/latent_alignment_simple_decomposable_attention_test.py
+++ tests/models/latent_alignment_simple_decomposable_attention_test.py
@@ -1,12 +1,11 @@
 # pylint: disable=invalid-name,no-self-use,protected-access
 from allennlp.common.testing import ModelTestCase
-
 from weak_supervision import *
 
 class LatentAlignmentDAMTest(ModelTestCase):
     def setUp(self):
         super().setUp()
-        self.set_up_model("fixtures/latent_alignment/experiment.json",
+        self.set_up_model("fixtures/latent_alignment/experiment_DAM.json",
                           "fixtures/data/wikitables/alignment_preprocessed.json")
 
     def test_model_can_train_save_and_load(self):
